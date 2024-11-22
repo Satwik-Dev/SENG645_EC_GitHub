58bff97d250a780270f35e7dd0fdd5ed1276fb3d
Satwik-Dev <satwika1@umbc.edu>
2024-11-21 23:32:50 -0500

    initial commit to version 1.01

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ package SENG645_EC_GitHub;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Second commit");
+        System.out.println("this the the version 1.01 branch");
     }
 }
